5e5f196f8ae49d3b9a644f16dd6a37715a8895ed
Trung252k2 <trungtannguyen44@gmail.com>
2025-09-11 09:55:12 +0700

    User Get Services and Detail

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import { verifyToken } from '~/middlewares/auth.middleware.js'
 import { serviceController } from '~/controllers/service.controller'
-import { serviceValidation } from '~/validations/service.validation'
+import { serviceValidation } from '~/validations/service.validation.js'
 
 const Router = express.Router()
 
-// CRUD + xem chi tiết
+// Public routes (không cần token)
+Router.get('/', serviceController.getAllServices)
+Router.get('/:id', serviceController.getServiceById)
+
+// Admin routes (cần token)
 Router.post('/', verifyToken, serviceValidation.createService, serviceController.createService)
-Router.get('/', verifyToken, serviceController.getAllServices)
-Router.get('/:id', verifyToken, serviceController.getServiceById)
 Router.put('/:id', verifyToken, serviceValidation.updateService, serviceController.updateService)
 Router.patch('/:id/hide', verifyToken, serviceController.hideService)
 Router.delete('/:id', verifyToken, serviceController.deleteService)

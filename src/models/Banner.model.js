cacfe91ca2ca5ee0e8a963e96dcf97461b77f13e
Trung252k2 <trungtannguyen44@gmail.com>
2025-09-24 20:26:48 +0700

    Fix: Banner (tạm ổn mai sửa tiếp)

--- src/models/Banner.model.js
+++ src/models/Banner.model.js
@@ -1,4 +1,3 @@
-import { number } from 'joi'
 import mongoose from 'mongoose'
 
 const schemaOptions = {
@@ -10,30 +9,43 @@ const schemaOptions = {
   }
 }
 
-const bannerSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    trim: true,
-    maxlength: 200
+const bannerSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      maxlength: 200
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    image: {
+      type: Object,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    position: {
+      type: Number,
+      default: 0,
+      enum: [0, 1, 2, 3, 4] // 0: no use
+    },
+    layout: {
+      type: Number,
+      enum: [1, 2, 3], // 1: option1, 2: option2, 3: option3
+      required: true
+    },
+    size: {
+      type: String,
+      enum: ['large', 'small'], // phân biệt banner lớn/nhỏ
+      default: 'large'
+    }
   },
-  description: {
-    type: String,
-    trim: true
-  },
-  image: {
-    type: Object,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true
-  },
-  position: {
-    type: Number,
-    default: 0,
-    enum: [0, 1, 2, 3, 4] // 0: no use
-  }
-}, schemaOptions)
+  schemaOptions
+)
 
 const BannerModel = mongoose.model('banners', bannerSchema)
 
--- src/validations/banner.validation.js
+++ src/validations/banner.validation.js
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError.js'
-import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators.js'
 
 const idValidationRule = Joi.object({
   id: Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
@@ -12,11 +12,13 @@ const createBanner = async (req, res, next) => {
     title: Joi.string().max(200).required(),
     description: Joi.string().max(500).required(),
     link: Joi.string().required(),
-    position: Joi.number().valid(0, 1, 2, 3, 4).required(), // 0: no use
-    image: Joi.array().items(Joi.string().uri()).optional()
+    position: Joi.number().valid(0, 1, 2, 3, 4).required(),
+    layout: Joi.number().valid(1, 2, 3).required(),
+    size: Joi.string().valid('large', 'small').optional(),
+    image: Joi.any().optional()
   })
   try {
-    const data = req?.body ? req.body : {}
+    const data = req.body || {}
     const validatedData = await createBannerRule.validateAsync(data, { abortEarly: false })
     req.body = validatedData
     next()
@@ -30,15 +32,15 @@ const updateBanner = async (req, res, next) => {
     title: Joi.string().optional(),
     description: Joi.string().optional(),
     link: Joi.string().optional(),
-    position: Joi.number().valid(0, 1, 2, 3, 4).optional() // 0: no use
+    position: Joi.number().valid(0, 1, 2, 3, 4).optional(),
+    layout: Joi.number().valid(1, 2, 3).optional(),
+    size: Joi.string().valid('large', 'small').optional()
   })
   try {
-    const data = req?.body ? req.body : {}
-    const params = req?.params ? req.params : {}
-    const validatedParams = await idValidationRule.validateAsync(params, { abortEarly: false })
-    const validatedData = await updateBannerRule.validateAsync(data, { abortEarly: false })
-    req.body = validatedData
-    req.params = validatedParams
+    const data = req.body || {}
+    const params = req.params || {}
+    req.params = await idValidationRule.validateAsync(params, { abortEarly: false })
+    req.body = await updateBannerRule.validateAsync(data, { abortEarly: false })
     next()
   } catch (error) {
     next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, error.message))
@@ -47,9 +49,8 @@ const updateBanner = async (req, res, next) => {
 
 const deleteBanner = async (req, res, next) => {
   try {
-    const params = req?.params ? req.params : {}
-    const validatedParams = await idValidationRule.validateAsync(params, { abortEarly: false })
-    req.params = validatedParams
+    const params = req.params || {}
+    req.params = await idValidationRule.validateAsync(params, { abortEarly: false })
     next()
   } catch (error) {
     next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, error.message))
